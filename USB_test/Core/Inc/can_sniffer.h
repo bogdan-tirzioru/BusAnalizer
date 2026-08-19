0a8944bc080dc410346b16b49c34f544240ebb1b
bogdan-tirzioru <52720412+bogdan-tirzioru@users.noreply.github.com>
2026-08-19 19:52:35 +0300

    Add selectable CAN1 bitrate profiles

--- USB_test/Core/Inc/can_sniffer.h
+++ USB_test/Core/Inc/can_sniffer.h
@@ -4,12 +4,17 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#define CAN_SNIFFER_BITRATE_250K 250000UL
+#define CAN_SNIFFER_BITRATE_500K 500000UL
+
 void CAN_Sniffer_Init(void);
 void CAN_Sniffer_Process(void);
 void CAN_Sniffer_Start(void);
 void CAN_Sniffer_Stop(void);
 void CAN_Sniffer_Clear(void);
 bool CAN_Sniffer_IsRunning(void);
+bool CAN_Sniffer_SetBitrate(uint32_t bitrate);
+uint32_t CAN_Sniffer_GetBitrate(void);
 
 uint32_t CAN_Sniffer_GetRxCount(void);
 uint32_t CAN_Sniffer_GetErrorCount(void);

7713006f8c5076475a92131478619f20f85a64ca
bogdan-tirzioru <52720412+bogdan-tirzioru@users.noreply.github.com>
2026-08-19 17:50:57 +0300

    USB_test: add vendor bulk class interface

---
+++ USB_test/USB_DEVICE/App/usbd_bulk.h
@@ -0,0 +1,36 @@
+#ifndef USBD_BULK_H
+#define USBD_BULK_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "usbd_def.h"
+
+#define BULK_IN_EP                 0x81U
+#define BULK_OUT_EP                0x01U
+#define BULK_FS_MAX_PACKET_SIZE    64U
+#define BULK_HS_MAX_PACKET_SIZE    512U
+#define BULK_TEST_BLOCK_SIZE       4096U
+
+typedef struct
+{
+  uint32_t tx_transfers;
+  uint32_t tx_bytes;
+  uint32_t rx_packets;
+  uint32_t rx_bytes;
+} USBD_BULK_StatsTypeDef;
+
+extern USBD_ClassTypeDef USBD_BULK;
+
+uint8_t USBD_BULK_Transmit(USBD_HandleTypeDef *pdev,
+                           uint8_t *buffer,
+                           uint32_t length);
+uint8_t USBD_BULK_TxReady(USBD_HandleTypeDef *pdev);
+void USBD_BULK_GetStats(USBD_BULK_StatsTypeDef *stats);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* USBD_BULK_H */

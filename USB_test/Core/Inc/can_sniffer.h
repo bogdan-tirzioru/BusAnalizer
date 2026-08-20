dfd51fc8197b631f5e8d6f10601dc12ef4529e74
bogdan-tirzioru <52720412+bogdan-tirzioru@users.noreply.github.com>
2026-08-20 11:18:18 +0300

    Add USB_test CAN FD data timing API

--- USB_test/Core/Inc/can_sniffer.h
+++ USB_test/Core/Inc/can_sniffer.h
@@ -4,8 +4,11 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#define CAN_SNIFFER_BITRATE_250K 250000UL
-#define CAN_SNIFFER_BITRATE_500K 500000UL
+#define CAN_SNIFFER_FDCAN_CLOCK_HZ 80000000UL
+#define CAN_SNIFFER_BITRATE_250K    250000UL
+#define CAN_SNIFFER_BITRATE_500K    500000UL
+#define CAN_SNIFFER_BITRATE_1M      1000000UL
+#define CAN_SNIFFER_DATA_BITRATE_5M 5000000UL
 
 void CAN_Sniffer_Init(void);
 void CAN_Sniffer_Process(void);
@@ -19,9 +22,15 @@ bool CAN_Sniffer_SetBitTiming(uint32_t prop_seg,
                               uint32_t phase_seg2,
                               uint32_t sjw,
                               uint32_t brp);
+bool CAN_Sniffer_SetDataBitTiming(uint32_t prop_seg,
+                                  uint32_t phase_seg1,
+                                  uint32_t phase_seg2,
+                                  uint32_t sjw,
+                                  uint32_t brp);
 bool CAN_Sniffer_StartListenOnly(void);
 void CAN_Sniffer_Reset(void);
 uint32_t CAN_Sniffer_GetBitrate(void);
+uint32_t CAN_Sniffer_GetDataBitrate(void);
 
 uint32_t CAN_Sniffer_GetRxCount(void);
 uint32_t CAN_Sniffer_GetErrorCount(void);

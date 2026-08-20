79726f5a570e3bb4b62884b7f35033d34460dcea
bogdan-tirzioru <52720412+bogdan-tirzioru@users.noreply.github.com>
2026-08-20 11:19:18 +0300

    Extend USB_test gs_usb ABI for CAN FD

--- USB_test/USB_DEVICE/App/usbd_bulk.h
+++ USB_test/USB_DEVICE/App/usbd_bulk.h
@@ -9,11 +9,14 @@ extern "C" {
 
 #include <stdint.h>
 
-#define GS_USB_IN_EP                 0x81U
-#define GS_USB_OUT_EP                0x01U
-#define GS_USB_FS_MAX_PACKET_SIZE    64U
-#define GS_USB_HS_MAX_PACKET_SIZE    512U
-#define GS_USB_HOST_FRAME_SIZE       20U
+#define GS_USB_IN_EP                         0x81U
+#define GS_USB_OUT_EP                        0x01U
+#define GS_USB_FS_MAX_PACKET_SIZE            64U
+#define GS_USB_HS_MAX_PACKET_SIZE            512U
+#define GS_USB_HOST_FRAME_HEADER_SIZE        12U
+#define GS_USB_CLASSIC_HOST_FRAME_SIZE       20U
+#define GS_USB_FD_HOST_FRAME_SIZE            76U
+#define GS_USB_HOST_FRAME_SIZE               GS_USB_FD_HOST_FRAME_SIZE
 
 enum
 {
@@ -23,7 +26,12 @@ enum
   GS_USB_BREQ_BERR = 3U,
   GS_USB_BREQ_BT_CONST = 4U,
   GS_USB_BREQ_DEVICE_CONFIG = 5U,
-  GS_USB_BREQ_TIMESTAMP = 6U
+  GS_USB_BREQ_TIMESTAMP = 6U,
+  GS_USB_BREQ_IDENTIFY = 7U,
+  GS_USB_BREQ_GET_USER_ID = 8U,
+  GS_USB_BREQ_SET_USER_ID = 9U,
+  GS_USB_BREQ_DATA_BITTIMING = 10U,
+  GS_USB_BREQ_BT_CONST_EXT = 11U
 };
 
 enum
@@ -33,7 +41,17 @@ enum
 };
 
 #define GS_CAN_MODE_LISTEN_ONLY      (1UL << 0)
+#define GS_CAN_MODE_FD               (1UL << 8)
+
 #define GS_CAN_FEATURE_LISTEN_ONLY   (1UL << 0)
+#define GS_CAN_FEATURE_FD            (1UL << 8)
+#define GS_CAN_FEATURE_BT_CONST_EXT  (1UL << 10)
+
+#define GS_CAN_FLAG_OVERFLOW         (1U << 0)
+#define GS_CAN_FLAG_FD               (1U << 1)
+#define GS_CAN_FLAG_BRS              (1U << 2)
+#define GS_CAN_FLAG_ESI              (1U << 3)
+
 #define GS_HOST_FRAME_ECHO_ID_RX     0xFFFFFFFFUL
 
 #define GS_CAN_EFF_FLAG              0x80000000UL
@@ -91,13 +109,35 @@ typedef struct GS_USB_PACKED
 
 typedef struct GS_USB_PACKED
 {
+  uint32_t feature;
+  uint32_t fclk_can;
+  uint32_t tseg1_min;
+  uint32_t tseg1_max;
+  uint32_t tseg2_min;
+  uint32_t tseg2_max;
+  uint32_t sjw_max;
+  uint32_t brp_min;
+  uint32_t brp_max;
+  uint32_t brp_inc;
+  uint32_t dtseg1_min;
+  uint32_t dtseg1_max;
+  uint32_t dtseg2_min;
+  uint32_t dtseg2_max;
+  uint32_t dsjw_max;
+  uint32_t dbrp_min;
+  uint32_t dbrp_max;
+  uint32_t dbrp_inc;
+} GS_USB_BitTimingConstExtended;
+
+typedef struct GS_USB_PACKED
+{
   uint32_t echo_id;
   uint32_t can_id;
   uint8_t can_dlc;
   uint8_t channel;
   uint8_t flags;
   uint8_t reserved;
-  uint8_t data[8];
+  uint8_t data[64];
 } GS_USB_HostFrame;
 
 extern USBD_ClassTypeDef USBD_GS_USB;

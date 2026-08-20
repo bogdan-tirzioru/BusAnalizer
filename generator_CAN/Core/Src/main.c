75d292573b21e866a6a9440b78f3e25eb2136bfa
bogdan-tirzioru <52720412+bogdan-tirzioru@users.noreply.github.com>
2026-08-20 09:17:21 +0300

    Restore generated assert comment

--- generator_CAN/Core/Src/main.c
+++ generator_CAN/Core/Src/main.c
@@ -819,7 +819,7 @@ void Error_Handler(void)
 }
 #ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file name
+  * @brief  Reports the name of the source file and the source line number
   *         where the assert_param error has occurred.
   * @param  file: pointer to the source file name
   * @param  line: assert_param error line source number

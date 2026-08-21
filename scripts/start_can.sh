eb906e1a7b6c0fa7e32200e6bddd6a37e088ad20
boti <bogdan_tirzioru@yahoo.com>
2026-08-21 09:24:28 +0300

    insert commands to check the info of connections

--- scripts/start_can.sh
+++ scripts/start_can.sh
@@ -4,3 +4,5 @@ ip link set can0 type can     bitrate 1000000 sample-point 0.875     dbitrate 50
 ip link set can1 type can     bitrate 1000000 sample-point 0.875     dbitrate 5000000 dsample-point 0.8125     fd on listen-only on
 ip link set can0 up
 ip link set can1 up
+ip -details -statistics link show can0
+ip -details -statistics link show can1
